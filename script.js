fca2a27bbc3606a7d046335d21645f0dfc4ba566
LeoBaumhart <Leonardo.2222583@aluno.faculdadedescomplica.com.br>
2022-06-20 21:53:49 -0300

    Adição de todos arquivos iniciais

---
+++ script.js
@@ -0,0 +1,96 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-produto')
+const sTipo = document.querySelector('#m-tipo')
+const sPreco = document.querySelector('#m-preco')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
+
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
+
+  if (edit) {
+    sNome.value = itens[index].produto
+    sTipo.value = itens[index].tipo
+    sPreco.value = itens[index].preco
+    id = index
+  } else {
+    sNome.value = ''
+    sTipo.value = ''
+    sPreco.value = ''
+  }
+  
+}
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function deleteItem(index) {
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>${item.produto}</td>
+    <td>${item.tipo}</td>
+    <td>R$ ${item.preco}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+  
+  if (sNome.value == '' || sTipo.value == '' || sPreco.value == '') {
+    return
+  }
+
+  e.preventDefault();
+
+  if (id !== undefined) {
+    itens[id].produto = sNome.value
+    itens[id].tipo = sTipo.value
+    itens[id].preco = sPreco.value
+  } else {
+    itens.push({'produto': sNome.value, 'tipo': sTipo.value, 'preco': sPreco.value})
+  }
+
+  setItensBD()
+
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
